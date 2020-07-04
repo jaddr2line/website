4907294cb3d8c3b8309a256a186498d290a20a02
Jaden Weiss <jaden@jadendw.dev>
2020-07-04 13:07:51 -0400

    update hugo

--- hugo/hugo.sh
+++ hugo/hugo.sh
@@ -4,7 +4,7 @@ set -e
 
 cd $(dirname $0)
 
-export HUGO_VERSION=0.55.6
+export HUGO_VERSION=0.73.0
 
 rm -f hugo.tar.gz
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_"$HUGO_VERSION"_Linux-64bit.tar.gz -O hugo.tar.gz
